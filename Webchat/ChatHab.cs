320e2d12854efaa269704c23fb639f44bb34b735
KatyaDemina <demina-ekaterina-88@mail.ru>
2023-02-23 12:24:05 +0300

    second

---
+++ Webchat/ChatHab.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webchat
+{
+    public class ChatHab : Hub
+    { public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Send", message, userName);//
+        }
+
+    }
+    
+    }
+
